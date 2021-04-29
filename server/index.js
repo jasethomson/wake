f732e449d8a2e79eeca1723dcf34efa20b5ae28a
jasethomson <jasethomson31@gmail.com>
2021-04-29 10:50:15 -0700

    added backend folder and verify.js

--- server/index.js
+++ server/index.js
@@ -2,3 +2,7 @@ require('dotenv/config');
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
+const verify = require("./backend/verify");
+
+console.log(verify);
