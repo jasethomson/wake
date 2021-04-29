4495bbe310c0dfcd458f9e657f30e9c9ecfa3fb8
jasethomson <jasethomson31@gmail.com>
2021-04-29 09:10:54 -0700

    added url func to parse and return youtube id

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -10,19 +10,44 @@ const Header = props => {
   return (
     <div className={`header`}>
       {props.title ? <h1>{props.title}</h1> : null}
-      {props.search ? search(props) : null}
+      {props.search ? <Search pageProps={props} /> : null}
     </div>
   );
 }
 
-const search = props => {
-  console.log(props.search.handleChange)
-  return (
-    <div className={`search`}>
-      <input type="text" onChange={props.search.handleChange} />
-      <i className="fas fa-search"></i>
-    </div>
-  )
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      value: '',
+      id: ''
+    }
+  }
+
+  handleChange(e) {
+    let value = e.target.value;
+    let id = this.props.pageProps.stateVal ? this.props.pageProps.stateVal(value) : '';
+    this.setState({ value, id });
+  }
+
+  handleSubmit() {
+    console.log(`${this.state.value}, with ${this.state.id} was submitted`);
+    event.preventDefault();
+  }
+
+  render() {
+    console.log("search", this.context);
+    this.context.Search = this.state;
+    return (
+      <form onSubmit={this.handleSubmit} className={`search`}>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <button className="btn-icon" type="submit"><i className="fas fa-search"></i></button>
+      </form>
+    )
+  }
 }
+Search.contextType = AppContext;
 
 export default Header;
--- app/pages/music.jsx
+++ app/pages/music.jsx
@@ -6,28 +6,23 @@ import Header from "../components/header";
 export default class Music extends React.Component {
   constructor(props) {
     super(props);
-    this.searchHandleChange = this.searchHandleChange.bind(this);
-    this.searchHandleSubmit = this.searchHandleSubmit.bind(this);
     this.state = {
       title: "Music"
     }
   }
 
-  searchHandleChange(event) {
-    console.log(event.currentTarget.value);
-  }
-
-  searchHandleSubmit(event) {
-    console.log(event);
+  getYoutubeId(url) {
+    let id = url.indexOf('.com') > 1 ? url.split('=')[1].split('&')[0] : '';
+    return id;
   }
 
   render() {
     console.log("music", this.context);
     this.context.Music = this.state;
-    let search = { handleChange: this.searchHandleChange, handleSubmit: this.searchHandleSubmit }
     return (
       <Header
-        search={search}
+        search
+        stateVal={this.getYoutubeId}
       />
     );
   }
