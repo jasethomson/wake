863cfd511e11d3d030e69190706d2f56dd81d25c
jasethomson <jasethomson31@gmail.com>
2021-04-29 11:06:47 -0700

    updated index.js

--- server/index.js
+++ server/index.js
@@ -6,3 +6,22 @@ const pool = require("./db");
 const verify = require("./backend/verify");
 
 console.log(verify);
+//middleware
+app.use(cors());
+//access to body req obj to get data from front end
+app.use(express.json()); //req.body
+
+//ROUTES//
+
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server has started on port 5432.");
+})
