5258da5a5e6a001f06dcc408febc9c8209efdb38
jasethomson <jasethomson31@gmail.com>
2021-04-29 19:05:30 -0700

    updated frontend to grab title, artist and set date of add to youtube videos added, added table component and rendered on music. set up header to reset state on search of a song

--- app/pages/home.jsx
+++ app/pages/home.jsx
@@ -14,10 +14,11 @@ export default class Home extends React.Component {
     console.log("home", this.context);
     this.context.Home = this.state;
     return (
-      <Header
-        title={this.state.title}
-      />
-
+      <div className="pageTop">
+        <Header
+          title={this.state.title}
+        />
+      </div>
     );
   }
 }
--- app/pages/music.jsx
+++ app/pages/music.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import AppContext from "../context";
 
 import Header from "../components/header";
+import Table from "../components/table";
 
 export default class Music extends React.Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
-      title: "Music"
+      title: "Music",
+      songs: [
+        {
+          title: "Shelter",
+          artist: "Birdy",
+          dateAdded: "04/29/2021"
+        }
+      ]
     }
   }
 
@@ -24,7 +32,23 @@ export default class Music extends React.Component {
   getYouTubeData(id) {
     fetch(`http://localhost:5000/tube?id=${id}`)
       .then(res => res.json())
-      .then(song => console.log(song));
+      .then(data => {
+        let song = {};
+        let songInfo = data.items[0].snippet.localized.title;
+        let ft = songInfo.indexOf('ft.') > 1 ? songInfo.split('ft.')[songInfo.split('ft.').length - 1] : '';
+        song.artist = songInfo.split('-', 1)[0];
+        song.artist += `ft. ${ft}`;
+        song.title = songInfo.split('-')[1];
+        song.title = song.title.slice(0, song.title.indexOf('('));
+        let dateObj = new Date();
+        let month = dateObj.getUTCMonth() + 1;
+        let day = dateObj.getUTCDate();
+        let year = dateObj.getUTCFullYear();
+        song.dateAdded = month + "/" + day + "/" + year;
+        this.setState({
+          songs: [...this.state.songs, song]
+        });
+      });
   }
 
   render() {
@@ -32,11 +56,17 @@ export default class Music extends React.Component {
     this.context.Music = this.state;
 
     return (
-      <Header
-        search
-        stateVal={this.getYoutubeId}
-        handleSubmit={this.handleSubmit}
-      />
+      <div className="pageTop">
+        <Header
+          search
+          stateVal={this.getYoutubeId}
+          handleSubmit={this.handleSubmit}
+        />
+        <Table
+          songs={this.state.songs}
+        />
+      </div>
+
     );
   }
 }
