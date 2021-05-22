3e086af47e09ab20364177e9d681bd7eee0a397e
jasethomson <jasethomson31@gmail.com>
2021-05-21 17:35:31 -0700

    converted app from class to function

--- app/app.jsx
+++ app/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, createContext } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,32 +11,25 @@ import Home from './pages/home';
 import Music from './pages/music';
 import Nav from './components/nav';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: "Wake"
-    }
-  }
-
-  render() {
-    const appContext = { App: this.state };
-    console.log(appContext);
-    return (
-      <AppContext.Provider value={appContext}>
-        <Router>
-          <header>
-            <div className="logoName">
-              <h3>Wake</h3>
-            </div>
-            <Nav />
-          </header>
-          <Switch>
-            <Route path="/music" component={Music} />
-            <Route path="/" component={Home} />
-          </Switch>
-        </Router>
-      </AppContext.Provider>
-    );
-  }
+const App = props => {
+  const appContext = { App: 'app was here' };
+  console.log("app", appContext);
+  return (
+    <AppContext.Provider value={appContext}>
+      <Router>
+        <header>
+          <div className="logoName">
+            <h3>Wake</h3>
+          </div>
+          <Nav />
+        </header>
+        <Switch>
+          <Route path="/music" component={Music} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
+    </AppContext.Provider>
+  );
 }
+
+export default App;
