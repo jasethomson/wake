b95bf7e9d0aedf9471d711e2f96505df0a11d1a0
jasethomson <jasethomson31@gmail.com>
2021-05-22 12:58:31 -0700

    converted table to function from class and created row func in table to map data into rows in table

--- app/components/table.jsx
+++ app/components/table.jsx
@@ -1,45 +1,41 @@
 import React from 'react';
-import AppContext from '../context';
 
-export default class Table extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: "Table"
-    }
-  }
-
-  render() {
-    let count = 0;
-    return (
-      <table>
-        <thead>
-          <tr>
-            {this.props.cols.map(col => {
-              return <th className={`td-${col.name}`} key={col.name}>{col.header}</th>
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.data ?
-            this.props.data.map(dataPiece => {
-              return (
-                <tr key={dataPiece[this.props.id]}>
-                  {this.props.cols.map(col => {
-                    if (col.name === "count") {
-                      return <td className="td-count" key={++count}>{count}</td>;
-                    } else if (col.name === "delete") {
-                      return <td className="td-icon" key={`${count}-delete`}><div onClick={() => this.props.delClick(dataPiece[this.props.id])}><i className="fas fa-minus-circle fa-lg"></i></div></td>;
-                    } else {
-                      return <td key={dataPiece[col.name]}>{dataPiece[col.name]}</td>;
-                    }
-                  })}
-                </tr>
-              );
-            }) : <div>No data</div>}
-        </tbody>
-      </table>
-    );
-  }
+const Table = props => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {props.cols.map(col => {
+            return <th className={`td-${col.name}`} key={col.name}>{col.header}</th>
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {props.data ? rows(props) : <div>No data</div>}
+      </tbody>
+    </table>
+  );
 }
-Table.contextType = AppContext;
+
+export default Table;
+
+const rows = props => {
+  let count = 0;
+  const rows = [];
+  props.data.map(dataPiece => {
+    rows.push(
+      <tr key={dataPiece[props.id]}>
+        {props.cols.map(col => {
+          if (col.name === "count") {
+            return <td className="td-count" key={++count}>{count}</td>;
+          } else if (col.name === "delete") {
+            return <td className="td-icon" key={`${count}-delete`}><div onClick={() => this.props.delClick(dataPiece[props.id])}><i className="fas fa-minus-circle fa-lg"></i></div></td>;
+          } else {
+            return <td key={dataPiece[col.name]}>{dataPiece[col.name]}</td>;
+          }
+        })}
+      </tr>
+    );
+  });
+  return rows;
+}
