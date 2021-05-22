64a28d8286c9c58051969ced9a30fbc85c4ee8b9
jasethomson <jasethomson31@gmail.com>
2021-05-22 11:56:04 -0700

    converted search class to a functional component using useState

--- app/components/search.jsx
+++ app/components/search.jsx
@@ -1,37 +1,30 @@
-import React from 'react';
+import React, { useContext, createContext, useState } from 'react';
+import AppContext from '../context';
 import { Link } from 'react-router-dom';
 
-export default class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {
-      value: '',
-      id: ''
-    }
-  }
+const Search = props => {
+  const [state, setState] = useState({ value: '', id: '' });
 
-  handleChange(e) {
+  const handleChange = e => {
     let value = e.target.value;
-    let id = this.props.pageProps.stateVal ? this.props.pageProps.stateVal(value) : '';
-    this.setState({ value, id });
+    let id = props.pageProps.stateVal ? props.pageProps.stateVal(e.target.value) : '';
+    setState(state => ({ value, id }));
   }
 
-  handleSubmit() {
+  const handleSubmit = () => {
     event.preventDefault();
-    this.props.pageProps.handleSubmit({ value: this.state.value, id: this.state.id });
-    this.setState({ value: '', id: '' });
+    props.pageProps.handleSubmit({ value: state.value, id: state.id });
+    setState(state => ({ value: '', id: '' }));
   }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className={`search`}>
-        <button className="btn-icon" type="submit">
-          <i className="fas fa-search" />
-        </button>
-        <input type="text" value={this.state.value} onChange={this.handleChange} />
-      </form>
-    )
-  }
+  return (
+    <form onSubmit={handleSubmit} className={`search`}>
+      <button className="btn-icon" type="submit">
+        <i className="fas fa-search" />
+      </button>
+      <input type="text" value={state.value} onChange={handleChange} />
+    </form>
+  );
 }
+
+export default Search;
